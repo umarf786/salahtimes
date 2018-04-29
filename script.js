58aac1c06e1b5eff19b53b5ee70e901527bca765
Umar Farooq <37244088+The-IT-Geek@users.noreply.github.com>
2018-04-29 21:26:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var data;
+var timestamp = Math.floor(Date.now()/100)
+if (!navigator.geolocation) {
+  alert("Geolocation is not supported by this browser!");
+}
+//This will make appear a pop up asking for permission
+navigator.geolocation.getCurrentPosition(showPosition, error);
+
+//In case the permission is granted
+function showPosition(position) {
+  data = position;
+  pos();
+}
+
+//In case the permission is denied
+function error() {
+  alert(
+    "Unable to retrieve your location! Allow the browser to track your location!"
+  );
+}
+var lat;
+var lon;
+function pos() {
+  lat = data.coords.latitude;
+  lon = data.coords.longitude;
+  console.log(data.coords.latitude);
+  console.log(data.coords.longitude);
+  console.log(timestamp);
+  $(document).ready(function(){
+    $.getJSON('http://api.aladhan.com/v1/timings/'+timestamp+'?latitude='+data.coords.latitude+'&longitude='+data.coords.longitude+'&method=3&school=1&tune=0,35,0,60,95,110,0,75,0', function(data1){
+        var fajr = data1.data.timings.Fajr;
+        var zuhur = data1.data.timings.Dhuhr;
+        var asr = data1.data.timings.Asr;
+        var maghrib = data1.data.timings.Maghrib;
+        var isha = data1.data.timings.Isha;
+        var name = data1;
+        $('.fajar').html(fajr);
+        $('.zuhur').html(zuhur);
+        $('.asr').html(asr);
+        $('.maghrib').html(maghrib);
+        $('.isha').html(isha);
+})
+})
+}
+
