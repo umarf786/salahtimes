7403dfbbabb51c28c2bbffbe0dd3d0d4ecae7d67
Umar Farooq <admin@DESKTOP-CUU7BND-4.local>
2021-02-04 05:42:20 +0000

    test

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ $.getJSON("2021.json", function(result){
   if(n=="Feb"){
     var feb = $(result.Feb);
     var data = feb;
-    console.log($(data._na));
+    console.log(data._4);
   } else{
     console.log("no")
   }
